dcb26c7c8879ca65270df942b2a04d0eefd4be7c
Matthew Horwood <matt@horwood.biz>
2023-07-18 12:59:31 +0100

    fix sed with cidr

--- config/start.sh
+++ config/start.sh
@@ -23,12 +23,12 @@ fi
 
 if [ -n "$allow_php_status_ip" ]
 then
-  sed -e "s/127.0.0.2/$allow_php_status_ip/" /config/nginx_site.conf > /etc/nginx/http.d/default.conf
+  sed -e "s!127.0.0.2!$allow_php_status_ip!" /config/nginx_site.conf > /etc/nginx/http.d/default.conf
 fi
 
 if [ -n "$allow_php_ping_ip" ]
 then
-  sed -e "s/127.0.0.3/$allow_php_ping_ip/"  /config/nginx_site.conf > /etc/nginx/http.d/default.conf
+  sed -e "s!127.0.0.3!$allow_php_ping_ip!"  /config/nginx_site.conf > /etc/nginx/http.d/default.conf
 fi
 
 if [ -n "$php_ping_text" ]
